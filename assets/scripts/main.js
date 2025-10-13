962e698403158e722791d15a6f0604db65e13744
Ima Laghima <imalaghima@gmail.com>
2025-10-13 17:53:41 +0500

    chore: replace indent 2 with 4

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,33 +1,33 @@
 function SetupContactButton() {
-  const contactButton = document.querySelector('.contacts_section');
-  if (!contactButton) return;
+    const contactButton = document.querySelector('.contacts_section');
+    if (!contactButton) return;
 
-  const contactButtonText = contactButton.querySelector('.contacts_section__text');
-  const contactButtonIcon = contactButton.querySelector('.contacts_section__icon');
+    const contactButtonText = contactButton.querySelector('.contacts_section__text');
+    const contactButtonIcon = contactButton.querySelector('.contacts_section__icon');
 
-  const checkmarkIconSrc = './assets/img/Check_Icon_SVG_White.svg';
-  const originalText = contactButtonText.textContent.trim();
-  const originalIconSrc = contactButtonIcon.src;
+    const checkmarkIconSrc = './assets/img/Check_Icon_SVG_White.svg';
+    const originalText = contactButtonText.textContent.trim();
+    const originalIconSrc = contactButtonIcon.src;
 
-  let timer = null;
+    let timer = null;
 
-  contactButton.addEventListener('click', () => {
-    navigator.clipboard.writeText(originalText);
-    contactButtonText.textContent = 'Copied!';
-    contactButtonIcon.src = checkmarkIconSrc;
+    contactButton.addEventListener('click', () => {
+        navigator.clipboard.writeText(originalText);
+        contactButtonText.textContent = 'Copied!';
+        contactButtonIcon.src = checkmarkIconSrc;
 
-    timer && clearTimeout(timer);
-    timer = setTimeout(() => {
-      contactButtonText.textContent = originalText;
-      contactButtonIcon.src = originalIconSrc;
-    }, 1000);
-  });
+        timer && clearTimeout(timer);
+        timer = setTimeout(() => {
+            contactButtonText.textContent = originalText;
+            contactButtonIcon.src = originalIconSrc;
+        }, 1000);
+    });
 }
 
 function Setup() {
-  SetupContactButton();
+    SetupContactButton();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  Setup();
+    Setup();
 });
